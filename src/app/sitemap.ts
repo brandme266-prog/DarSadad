e3914cecfb2aa8245ba77e63657bb5fb1afff52b
mohamed elkheateb <156317154+mohamed10987@users.noreply.github.com>
2026-09-02 14:24:50 +0200

    fix(sitemap): update articles import path

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from 'next';
-import { articles } from './blog/page';
+import { articles } from '@/data/articlesList';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://brand1me.com';
